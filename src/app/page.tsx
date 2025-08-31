6a9cb56a19b2ebe7f28f84bb64c7a2861fcabda9
yuvdwi <yuvdwi@gmail.com>
2025-08-31 06:57:58 -0400

    Fix moving squares to stay square and make grid span full width properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
         {/* Grid Background with Moving Blocks */}
         <div className="absolute inset-0">
           {/* SVG Grid */}
-          <svg className="w-full h-full opacity-40" viewBox="0 0 400 240">
+          <svg className="w-full h-full opacity-40" preserveAspectRatio="none">
             <defs>
               <pattern id="grid" width="20" height="20" patternUnits="userSpaceOnUse">
                 <path d="M 20 0 L 0 0 0 20" fill="none" stroke="rgb(168 85 247)" strokeWidth="0.9" opacity="0.6"/>
@@ -206,7 +206,7 @@ export default function Home() {
       <section id="how-it-works" className="relative py-20 bg-black overflow-hidden">
         {/* Grid Background */}
         <div className="absolute inset-0">
-          <svg className="w-full h-full opacity-40" viewBox="0 0 400 240">
+          <svg className="w-full h-full opacity-40" preserveAspectRatio="none">
             <defs>
               <pattern id="grid-how-it-works" width="20" height="20" patternUnits="userSpaceOnUse">
                 <path d="M 20 0 L 0 0 0 20" fill="none" stroke="rgb(168 85 247)" strokeWidth="0.9" opacity="0.6"/>
